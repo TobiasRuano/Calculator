6c24c4e7165be4f1fc76d3eb4d049ce1a5b76eaa
Tobias <ruano.t10@gmail.com>
2019-03-27 11:57:53 -0300

    Calculator buttons now change background alpha when tapped

--- Calculator/CustomButton.swift
+++ Calculator/CustomButton.swift
@@ -25,4 +25,10 @@ class CustomButton: UIButton {
         backgroundColor = UIColor.init(displayP3Red: 216/255, green: 216/255, blue: 216/255, alpha: 0.55)
         setTitleColor(.white, for: .normal)
     }
+    
+    override var isHighlighted: Bool {
+        didSet {
+            backgroundColor = isHighlighted ? UIColor.init(displayP3Red: 216/255, green: 216/255, blue: 216/255, alpha: 0) : UIColor.init(displayP3Red: 216/255, green: 216/255, blue: 216/255, alpha: 0.55)
+        }
+    }
 }
